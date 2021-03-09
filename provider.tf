12154b4014aa2450ab3da308ddd69a61103f3f18
JackPawsey <jfrewer@live.co.uk>
2021-03-09 14:50:42 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,8 @@
-#terraform {
-#  required_version = ">= 0.12"
-#}
-
-#provider "aws" {
-#  region  = "eu-west-1" #var.region
-#  version = "~> 3.24.0"
-#}
+terraform {
+  required_version = ">= 0.12"
+}
 
 provider "aws" {
-  region = "eu-west-1"
+  region  = "eu-west-1" #var.region
+  version = "~> 3.24.0"
 }
